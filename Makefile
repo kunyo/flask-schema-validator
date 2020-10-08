5120951a5005825ea98b62e07c597d0b77d9bf47
kn <kntrashcan@gmail.com>
2020-10-08 23:44:32 +0000

    Added formatter

--- Makefile
+++ Makefile
@@ -6,6 +6,7 @@ dep: clean
 	virtualenv --python=python3.8 ./venv
 	./venv/bin/python setup.py install
 	./venv/bin/pip freeze > requirements.txt
+	./venv/bin/pip install -r requirements-dev.txt	
 
 clean:
 	rm -rf ./build
--- flask_schema_validator/__init__.py
+++ flask_schema_validator/__init__.py
@@ -4,23 +4,24 @@ import jsonschema
 from flask import request
 from functools import wraps
 
-def validate(f=None, schema:dict=None):
-    if f is not None and schema is not None:  # pragma: no cover
-        raise ValueError(
-            'data and schema are the only supported arguments')
 
-    def validate_internal(f):
-        @wraps(f)
-        def decorated(*args, **kwargs):
-            if request.method != 'OPTIONS':  # pragma: no cover
-                if not request.is_json:
-                    return 'Invalid request', 400
+def validate(f=None, schema: dict = None):
+  if f is not None and schema is not None:  # pragma: no cover
+    raise ValueError(
+        'data and schema are the only supported arguments')
 
-                try:
-                    jsonschema.validate(instance=request.json, schema=schema)
-                except jsonschema.exceptions.ValidationError as err:
-                    return 'Schema validation failed: %s' % err.message, 400
-            return f(*args, **kwargs)
-        return decorated
+  def validate_internal(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+      if request.method != 'OPTIONS':  # pragma: no cover
+        if not request.is_json:
+          return 'Invalid request', 400
 
-    return validate_internal
+        try:
+          jsonschema.validate(instance=request.json, schema=schema)
+        except jsonschema.exceptions.ValidationError as err:
+          return 'Schema validation failed: %s' % err.message, 400
+      return f(*args, **kwargs)
+    return decorated
+
+  return validate_internal
